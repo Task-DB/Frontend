b97afccdda9c81c4a6cedb372408c8e14a008144
J1min <aoiharu41@gmail.com>
2022-12-14 13:05:09 +0900

    Add: dependency

--- next.config.js
+++ next.config.js
@@ -1,18 +1,9 @@
 module.exports = {
-  images: {
-    domains: [
-      "localhost",
-      "s3.ap-northeast-2.amazonaws.com",
-      "avatars.githubusercontent.com",
-      "taskdb.s3.ap-northeast-2.amazonaws.com",
-    ],
-    imageSizes: [256, 256, 256, 256, 256, 256, 256, 384],
-  },
-  rewrites() {
+  async rewrites() {
     return [
       {
-        source: "/:path*", //api request path
-        destination: "http://192.168.10.103:8092/:path*", //목적 path
+        source: "/:path*",
+        destination: "http://192.168.10.103:8092/:path*",
       },
     ];
   },
